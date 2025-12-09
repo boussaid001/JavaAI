56a7693bb08c0a13b4e29e29129e1ce5e687b742
boussaid001 <mohamedamine.boussaid@polytechnicien.tn>
2025-12-09 11:48:53 +0100

    fixing issues

--- src/main/java/com/polytech/javaai/chatCrontroller/ChatController.java
+++ src/main/java/com/polytech/javaai/chatCrontroller/ChatController.java
@@ -1,8 +1,12 @@
 package com.polytech.javaai.chatController;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -10,16 +14,20 @@ public class ChatController {
     private final ChatClient chatClient;
 
     // Constructor
-    public ChatController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+    public ChatController(ChatClient.Builder builder, ChatMemory memory) {
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(memory).build())
+                .build();
     }
 
+
+
     @GetMapping("/ask")
-    public String ask(@RequestParam String message) {
+    public Flux<String> ask(@RequestParam String message) {
         return chatClient
                 .prompt()   // start prompt
                 .user(message)  // provide user message
-                .call()     // execute call
+                .stream()     // execute call
                 .content(); // get content
     }
 }
